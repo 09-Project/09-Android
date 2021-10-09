ce3d5b967e54acc0a5208c2bec3ce89dbac62cf4
limsaehyun <sh00710000@gmail.com>
2021-10-09 15:28:00 +0900

    Feat :: Splash

---
+++ 09-Android/app/src/main/java/com/saehyun/a09_android/ui/activity/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.saehyun.a09_android.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.saehyun.a09_android.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
