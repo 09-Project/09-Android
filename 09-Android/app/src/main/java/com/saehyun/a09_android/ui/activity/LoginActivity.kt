86ed4bdf457ccf0663995b9a223e85f34c4bf88f
limsaehyun <sh00710000@gmail.com>
2021-10-13 16:14:52 +0900

    Feat :: LoginActivity

---
+++ 09-Android/app/src/main/java/com/saehyun/a09_android/ui/activity/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.saehyun.a09_android.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.saehyun.a09_android.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
