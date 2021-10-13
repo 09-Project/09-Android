e277e9a3ff36cf9e9e0cf522b89fa5e99eba22a5
limsaehyun <sh00710000@gmail.com>
2021-10-13 16:15:19 +0900

    Feat :: Splash effect

--- 09-Android/app/src/main/java/com/saehyun/a09_android/ui/activity/SplashActivity.kt
+++ 09-Android/app/src/main/java/com/saehyun/a09_android/ui/activity/SplashActivity.kt
@@ -1,12 +1,22 @@
 package com.saehyun.a09_android.ui.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.saehyun.a09_android.R
 
 class SplashActivity : AppCompatActivity() {
+
+    private var handler = Handler()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        handler.postDelayed({
+            val intent = Intent(baseContext, LoginActivity::class.java)
+            startActivity(intent)
+            finish() }, 2000)
     }
 }
