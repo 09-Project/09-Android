ce2cbd66df4667632e3fc4eafd1ad508d7a192b8
limsaehyun <sh00710000@gmail.com>
2021-10-15 08:09:25 +0900

    Feat :: Register

--- 09-Android/app/src/main/java/com/saehyun/a09_android/ui/activity/RegisterActivity.kt
+++ 09-Android/app/src/main/java/com/saehyun/a09_android/ui/activity/RegisterActivity.kt
@@ -2,6 +2,7 @@ package com.saehyun.a09_android.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.saehyun.a09_android.R
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
