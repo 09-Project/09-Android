79cf55486f7ec135eabaf323d35f3e945e8d2892
limsaehyun <sh00710000@gmail.com>
2021-10-15 08:08:46 +0900

    Feat :: startAnimation

--- 09-Android/app/src/main/java/com/saehyun/a09_android/ui/activity/SplashActivity.kt
+++ 09-Android/app/src/main/java/com/saehyun/a09_android/ui/activity/SplashActivity.kt
@@ -4,19 +4,46 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.annotation.UiThread
 import com.saehyun.a09_android.R
+import com.saehyun.a09_android.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
 
-    private var handler = Handler()
+    private lateinit var binding: ActivitySplashBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        handler.postDelayed({
-            val intent = Intent(baseContext, LoginActivity::class.java)
-            startActivity(intent)
-            finish() }, 2000)
+        splashAnimation()
+    }
+
+    @UiThread
+    private fun splashAnimation() {
+        val textAnim = AnimationUtils.loadAnimation(this, R.anim.anim_splash_textview)
+        binding.tvLogo.startAnimation(textAnim)
+        binding.tvLore.startAnimation(textAnim)
+        val imageAnim = AnimationUtils.loadAnimation(this, R.anim.anim_splash_imageview)
+        binding.ivLogo.startAnimation(imageAnim)
+
+        imageAnim.setAnimationListener(object : Animation.AnimationListener {
+
+            override fun onAnimationEnd(animation: Animation) {
+                startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+                overridePendingTransition(R.anim.anim_splash_out_top, R.anim.anim_splash_in_down)
+                finish()
+            }
+
+            override fun onAnimationStart(animation: Animation) {
+            }
+
+            override fun onAnimationRepeat(animation: Animation) {
+            }
+        })
     }
 }
