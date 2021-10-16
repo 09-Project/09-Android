97c9847e9f7c0d9de6bcbb6a6ebb5a34bed1aa46
saehyun <sh00710000@gmail.com>
2021-10-16 23:02:03 +0900

    Feat :: ActivityLoginBinding

--- 09-Android/app/src/main/java/com/saehyun/a09_android/ui/activity/LoginActivity.kt
+++ 09-Android/app/src/main/java/com/saehyun/a09_android/ui/activity/LoginActivity.kt
@@ -3,11 +3,15 @@ package com.saehyun.a09_android.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.saehyun.a09_android.R
+import com.saehyun.a09_android.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
