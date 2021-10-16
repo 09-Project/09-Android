e64d4c195685dd13f4912563f070550a8bddc2a6
saehyun <sh00710000@gmail.com>
2021-10-16 23:01:08 +0900

    Feat :: ActivityMainBinding

--- 09-Android/app/src/main/java/com/saehyun/a09_android/ui/activity/MainActivity.kt
+++ 09-Android/app/src/main/java/com/saehyun/a09_android/ui/activity/MainActivity.kt
@@ -3,10 +3,15 @@ package com.saehyun.a09_android.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.saehyun.a09_android.R
+import com.saehyun.a09_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
