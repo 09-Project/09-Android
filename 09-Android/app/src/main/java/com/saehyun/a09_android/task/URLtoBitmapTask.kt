c3576175871d1e16fb84df53057b4d2659030f2e
limsaehyun <sh00710000@gmail.com>
2021-10-25 19:27:46 +0900

    Feat :: URL to Bitmap

---
+++ 09-Android/app/src/main/java/com/saehyun/a09_android/task/URLtoBitmapTask.kt
@@ -0,0 +1,22 @@
+package com.saehyun.a09_android.task
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.AsyncTask
+import java.net.URL
+
+class URLtoBitmapTask() : AsyncTask<Void, Void, Bitmap>() {
+
+    lateinit var url: URL
+    override fun doInBackground(vararg params: Void?): Bitmap {
+        val bitmap = BitmapFactory.decodeStream(url.openStream())
+        return bitmap
+    }
+    override fun onPreExecute() {
+        super.onPreExecute()
+
+    }
+    override fun onPostExecute(result: Bitmap) {
+        super.onPostExecute(result)
+    }
+}
