440455f7e31fd99d1b8c4b861d689536b1381544
limsaehyun <sh00710000@gmail.com>
2021-10-26 23:14:04 +0900

    Feat :: Recyclerview setting

--- 09-Android/app/src/main/java/com/saehyun/a09_android/ui/activity/MainActivity.kt
+++ 09-Android/app/src/main/java/com/saehyun/a09_android/ui/activity/MainActivity.kt
@@ -5,8 +5,11 @@ import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import com.saehyun.a09_android.R
 import com.saehyun.a09_android.databinding.ActivityMainBinding
+import com.saehyun.a09_android.model.data.RcProductRvData
+import com.saehyun.a09_android.remote.RcProductRvAdapter
 import com.saehyun.a09_android.repository.Repository
 import com.saehyun.a09_android.util.REFRESH_TOKEN
 import com.saehyun.a09_android.util.ToastUtil
@@ -19,16 +22,23 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var reissueViewModel: ReissueViewModel
 
+    private var productList = arrayListOf<RcProductRvData>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.rvMainRcProduct.adapter = RcProductRvAdapter(applicationContext, productList)
+        binding.rvMainRcProduct.layoutManager = GridLayoutManager(this, 2)
+        binding.rvMainRcProduct.setHasFixedSize(true)
+        binding.rvMainRcProduct.adapter?.notifyDataSetChanged()
+
         val repository = Repository()
         val reissueViewModelFactory = ReissueViewModelFactory(repository)
         reissueViewModel = ViewModelProvider(this, reissueViewModelFactory).get(ReissueViewModel::class.java)
-        
+
         binding.button.setOnClickListener {
             reissueViewModel.authReissue(REFRESH_TOKEN)
             reissueViewModel.authReissueResponse.observe(this, Observer {
