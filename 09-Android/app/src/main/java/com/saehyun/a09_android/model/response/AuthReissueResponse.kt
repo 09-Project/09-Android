4c4b8ead9ddd48303ba3cfa4a88807629859c4d7
limsaehyun <sh00710000@gmail.com>
2021-10-26 20:15:35 +0900

    Feat :: SerializedName

--- 09-Android/app/src/main/java/com/saehyun/a09_android/model/response/AuthReissueResponse.kt
+++ 09-Android/app/src/main/java/com/saehyun/a09_android/model/response/AuthReissueResponse.kt
@@ -2,7 +2,7 @@ package com.saehyun.a09_android.model.response
 
 import com.google.gson.annotations.SerializedName
 
-data class AuthLoginResponse(
+data class AuthReissueResponse(
     @SerializedName("access_token")
     val accessToken: String,
     @SerializedName("refresh_token")
