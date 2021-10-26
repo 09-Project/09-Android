d23530bbfa27c15a87154373d5db9000d8061e3f
limsaehyun <sh00710000@gmail.com>
2021-10-26 20:16:55 +0900

    Fix :: lateinit var -> var

--- 09-Android/app/src/main/java/com/saehyun/a09_android/util/Constans.kt
+++ 09-Android/app/src/main/java/com/saehyun/a09_android/util/Constans.kt
@@ -2,5 +2,5 @@ package com.saehyun.a09_android.util
 
 val BASE_URL = "http://3.36.26.221:8080/"
 
-lateinit var ACCESS_TOKEN: String
-lateinit var REFRESH_TOKEN: String
+var ACCESS_TOKEN = "default"
+var REFRESH_TOKEN = "default"
