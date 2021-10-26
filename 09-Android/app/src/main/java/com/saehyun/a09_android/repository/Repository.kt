45e926edd760dc7211b9002f0f1e7234c7e95e2b
limsaehyun <sh00710000@gmail.com>
2021-10-26 19:33:23 +0900

    Feat :: Repository

--- 09-Android/app/src/main/java/com/saehyun/a09_android/repository/Repository.kt
+++ 09-Android/app/src/main/java/com/saehyun/a09_android/repository/Repository.kt
@@ -1,4 +1,12 @@
 package com.saehyun.a09_android.repository
 
+import com.saehyun.a09_android.model.request.AuthSignUpRequest
+import com.saehyun.a09_android.network.ApiProvider
+import retrofit2.Response
+
 class Repository {
+    suspend fun authSignUP(name : String, username : String, password : String) : Response<Void> {
+        val authSignUpRequest = AuthSignUpRequest(name, username, password)
+        return ApiProvider.api.authSignUp(authSignUpRequest)
+    }
 }
