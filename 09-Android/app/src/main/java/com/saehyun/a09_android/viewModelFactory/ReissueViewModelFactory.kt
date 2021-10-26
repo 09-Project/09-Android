34b677d3511ff25fed2e63a7832d13bc4a812c24
limsaehyun <sh00710000@gmail.com>
2021-10-26 20:16:34 +0900

    Feat :: Reissue ViewModelFactory

---
+++ 09-Android/app/src/main/java/com/saehyun/a09_android/viewModelFactory/ReissueViewModelFactory.kt
@@ -0,0 +1,17 @@
+package com.saehyun.a09_android.viewModelFactory
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.saehyun.a09_android.repository.Repository
+import com.saehyun.a09_android.viewModel.LoginViewModel
+import com.saehyun.a09_android.viewModel.RegisterViewModel
+import com.saehyun.a09_android.viewModel.ReissueViewModel
+
+class ReissueViewModelFactory(
+    private val repository : Repository
+    ) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return ReissueViewModel(repository) as T
+    }
+}
