e4c3211b14f2bdf4a0856a92b39058179d189868
limsaehyun <sh00710000@gmail.com>
2021-10-26 23:13:52 +0900

    Feat :: RcProductRvAdapter

--- 09-Android/app/src/main/java/com/saehyun/a09_android/remote/RcProductRvAdapter.kt
+++ 09-Android/app/src/main/java/com/saehyun/a09_android/remote/RcProductRvAdapter.kt
@@ -1,4 +1,45 @@
 package com.saehyun.a09_android.remote
 
-class RcProductRvAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
+import com.saehyun.a09_android.R
+import com.saehyun.a09_android.model.data.RcProductRvData
+
+class RcProductRvAdapter(val context: Context, val productData: ArrayList<RcProductRvData>):
+    RecyclerView.Adapter<RcProductRvAdapter.Holder>(){
+
+    inner class Holder(itemView: View?) : RecyclerView.ViewHolder(itemView!!) {
+        val clRecommendedProduct = itemView?.findViewById<ConstraintLayout>(R.id.clRecommendedProduct)
+        val ivImage = itemView?.findViewById<ImageView>(R.id.ivImage)
+        val ivPurpose = itemView?.findViewById<ImageView>(R.id.ivPurpose)
+        val tvTitle = itemView?.findViewById<TextView>(R.id.tvTitle)
+        val tvTransactionRegion = itemView?.findViewById<TextView>(R.id.tvTransactionRegion)
+        val tvPrice = itemView?.findViewById<TextView>(R.id.tvPrice)
+
+
+        fun bind(rcProductRvData: RcProductRvData, context: Context) {
+            tvTitle?.text = rcProductRvData.title
+            tvTransactionRegion?.text = rcProductRvData.transaction_region
+            tvPrice?.text = rcProductRvData.price
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view = LayoutInflater.from(context).inflate(R.layout.recommended_product_view, parent, false)
+        return Holder(view)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bind(productData[position], context)
+    }
+
+    override fun getItemCount(): Int {
+        return productData.size
+    }
 }
