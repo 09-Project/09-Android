0311175d8c97b585ef202d732d78cee19f6d5502
limsaehyun <sh00710000@gmail.com>
2021-10-27 19:44:47 +0900

    Feat :: RcProductRvAdapter

--- 09-Android/app/src/main/java/com/saehyun/a09_android/remote/RcProductRvAdapter.kt
+++ 09-Android/app/src/main/java/com/saehyun/a09_android/remote/RcProductRvAdapter.kt
@@ -1,21 +1,31 @@
 package com.saehyun.a09_android.remote
 
+import android.app.ActionBar
 import android.content.Context
+import android.graphics.Bitmap
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.widget.AppCompatDrawableManager.preload
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.saehyun.a09_android.R
 import com.saehyun.a09_android.model.data.RcProductRvData
+import com.saehyun.a09_android.model.response.PostResponse
+import com.saehyun.a09_android.task.URLtoBitmapTask
+import java.net.URL
 
-class RcProductRvAdapter(val context: Context, val productData: ArrayList<RcProductRvData>):
-    RecyclerView.Adapter<RcProductRvAdapter.Holder>(){
+class RcProductRvAdapter(val context: Context, val productData: List<PostResponse>):
+    RecyclerView.Adapter<RcProductRvAdapter.Holder>() {
+
 
     inner class Holder(itemView: View?) : RecyclerView.ViewHolder(itemView!!) {
-        val clRecommendedProduct = itemView?.findViewById<ConstraintLayout>(R.id.clRecommendedProduct)
+        val clRecommendedProduct =
+            itemView?.findViewById<ConstraintLayout>(R.id.clRecommendedProduct)
         val ivImage = itemView?.findViewById<ImageView>(R.id.ivImage)
         val ivPurpose = itemView?.findViewById<ImageView>(R.id.ivPurpose)
         val tvTitle = itemView?.findViewById<TextView>(R.id.tvTitle)
@@ -23,15 +33,16 @@ class RcProductRvAdapter(val context: Context, val productData: ArrayList<RcProd
         val tvPrice = itemView?.findViewById<TextView>(R.id.tvPrice)
 
 
-        fun bind(rcProductRvData: RcProductRvData, context: Context) {
-            tvTitle?.text = rcProductRvData.title
-            tvTransactionRegion?.text = rcProductRvData.transaction_region
-            tvPrice?.text = rcProductRvData.price
+        fun bind(postResponse: PostResponse, context: Context) {
+            tvTitle?.text = postResponse.title
+            tvTransactionRegion?.text = postResponse.transaction_region
+            tvPrice?.text = postResponse.price
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-        val view = LayoutInflater.from(context).inflate(R.layout.recommended_product_view, parent, false)
+        val view =
+            LayoutInflater.from(context).inflate(R.layout.recommended_product_view, parent, false)
         return Holder(view)
     }
 
