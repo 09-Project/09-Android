edfab6786ae1da921ae7602ce8c9c534d8b1c96f
limsaehyun <sh00710000@gmail.com>
2021-10-30 16:46:59 +0900

    Feat :: binding

--- 09-Android/app/src/main/java/com/saehyun/a09_android/ui/activity/SearchActivity.kt
+++ 09-Android/app/src/main/java/com/saehyun/a09_android/ui/activity/SearchActivity.kt
@@ -2,10 +2,16 @@ package com.saehyun.a09_android.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.saehyun.a09_android.databinding.ActivitySearchBinding
 
 class SearchActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySearchBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search)
+
+        binding = ActivitySearchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
