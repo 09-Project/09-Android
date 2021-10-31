04366111f8534fa4bb1e556b2c54258c621b66cb
limsaehyun <sh00710000@gmail.com>
2021-10-31 23:22:53 +0900

    Feat :: PostOtherViewModel

---
+++ 09-Android/app/src/main/java/com/saehyun/a09_android/viewModel/PostOtherViewModel.kt
@@ -0,0 +1,23 @@
+package com.saehyun.a09_android.viewModel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.saehyun.a09_android.model.request.AuthLoginRequest
+import com.saehyun.a09_android.model.response.AuthLoginResponse
+import com.saehyun.a09_android.model.response.PostOtherResponse
+import com.saehyun.a09_android.model.response.PostResponse
+import com.saehyun.a09_android.repository.Repository
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+class PostOtherViewModel(private val repository: Repository) : ViewModel() {
+    val authPostOtherResponse : MutableLiveData<Response<PostOtherResponse>> = MutableLiveData()
+
+    fun authPostOther(page: Int, size: Int) {
+        viewModelScope.launch {
+            val response = repository.postOther()
+            authPostOtherResponse.value = response
+        }
+    }
+}
