b209995757b947b8be7b551d6652c8aa70dd12e2
limsaehyun <sh00710000@gmail.com>
2021-11-01 10:41:49 +0900

    Feat :: PostSearch

--- 09-Android/app/src/main/java/com/saehyun/a09_android/ui/activity/SearchActivity.kt
+++ 09-Android/app/src/main/java/com/saehyun/a09_android/ui/activity/SearchActivity.kt
@@ -2,16 +2,114 @@ package com.saehyun.a09_android.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import androidx.core.view.GravityCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import com.saehyun.a09_android.R
 import com.saehyun.a09_android.databinding.ActivitySearchBinding
+import com.saehyun.a09_android.model.data.PostValue
+import com.saehyun.a09_android.remote.RcProductRvAdapter
+import com.saehyun.a09_android.repository.Repository
+import com.saehyun.a09_android.util.ToastUtil
+import com.saehyun.a09_android.util.VIEW_SIZE
+import com.saehyun.a09_android.viewModel.PostSearchViewModel
+import com.saehyun.a09_android.viewModelFactory.PostSearchViewModelFactory
 
 class SearchActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySearchBinding
+
+    private lateinit var searchViewModel: PostSearchViewModel
+
+    private var productList = arrayListOf<PostValue>()
+
+    private var currentPage = 0
+    private var maxPage = 1
+
+    private lateinit var keyword : String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivitySearchBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val repository = Repository()
+
+        keyword = intent.getStringExtra("keyword").toString()
+
+        // Recyclerview Set
+        binding.srvMainRcProduct.layoutManager = GridLayoutManager(this, 2)
+        binding.srvMainRcProduct.setHasFixedSize(true)
+        binding.srvMainRcProduct.adapter = RcProductRvAdapter(applicationContext, productList)
+
+        // Page Set
+        binding.ibSearchPageBack.setOnClickListener {
+            --currentPage
+            if(currentPage <= 1) {
+                ToastUtil.print(applicationContext, "첫 페이지 입니다.")
+                return@setOnClickListener
+            }
+            searchViewModel.authPostSearch(keyword, currentPage, VIEW_SIZE)
+        }
+
+        binding.ibSearchPageNext.setOnClickListener {
+            ++currentPage
+            if(currentPage >= maxPage) {
+                currentPage = 0
+            }
+            searchViewModel.authPostSearch(keyword, currentPage, VIEW_SIZE)
+        }
+
+
+        val postSearchViewModelFactory = PostSearchViewModelFactory(repository)
+        searchViewModel = ViewModelProvider(this, postSearchViewModelFactory).get(PostSearchViewModel::class.java)
+
+        searchViewModel.authPostSearchResponse.observe(this, Observer {
+            if (it.isSuccessful) {
+                val size = it.body()!!.posts.size
+
+                val count = it.body()!!.count
+                maxPage = count / 16
+                if(count % 16 != 0) maxPage++
+                binding.tvSearchCurrentPage.text = "${currentPage+1}"
+                binding.tvSearchMaxPage.text = " / ${maxPage}"
+
+                productList.clear()
+                for(i: Int in 0 until size) {
+                    val postValue: PostValue = it.body()!!.posts.get(i)
+                    productList.add(postValue)
+                    binding.srvMainRcProduct.adapter?.notifyDataSetChanged()
+                }
+            } else {
+                when(it.code()) {
+                    404 -> ToastUtil.print(applicationContext,"이미지가 존재하지 않습니다")
+                }
+            }
+        })
+
+        searchViewModel.authPostSearch(keyword, currentPage, VIEW_SIZE)
+
+//        // Drawer Menu
+//        binding.ibMainMenu.setOnClickListener {
+//            if(binding.mainDrawer.isDrawerOpen(Gravity.RIGHT)) {
+//                binding.mainDrawer.closeDrawer(Gravity.RIGHT)
+//            } else {
+//                binding.mainDrawer.openDrawer(Gravity.RIGHT)
+//            }
+//        }
+//
+//        binding.mainNavi.setNavigationItemSelectedListener {
+//            when(it.itemId) {
+//                R.id.menuHome -> ToastUtil.print(applicationContext, "1")
+//                R.id.menuMyPage -> ToastUtil.print(applicationContext, "2")
+//                R.id.menuPost -> ToastUtil.print(applicationContext, "3")
+//            }
+//
+//            binding.mainDrawer.closeDrawer(GravityCompat.END)
+//            true
+//        }
     }
 }
