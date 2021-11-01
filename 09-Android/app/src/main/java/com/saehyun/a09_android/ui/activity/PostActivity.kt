b0505c3c08427083acb8cffc7344ccbc82b5a1e6
limsaehyun <sh00710000@gmail.com>
2021-11-01 20:03:37 +0900

    Feat :: PostOther

--- 09-Android/app/src/main/java/com/saehyun/a09_android/ui/activity/PostActivity.kt
+++ 09-Android/app/src/main/java/com/saehyun/a09_android/ui/activity/PostActivity.kt
@@ -2,14 +2,22 @@ package com.saehyun.a09_android.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import com.bumptech.glide.Glide
 import com.saehyun.a09_android.databinding.ActivityPostBinding
+import com.saehyun.a09_android.model.data.PostValue
+import com.saehyun.a09_android.model.response.PostOtherResponse
+import com.saehyun.a09_android.remote.RcOtherRvAdapter
+import com.saehyun.a09_android.remote.RcProductRvAdapter
 import com.saehyun.a09_android.repository.Repository
 import com.saehyun.a09_android.util.ToastUtil
 import com.saehyun.a09_android.viewModel.PostGetViewModel
+import com.saehyun.a09_android.viewModel.PostOtherViewModel
 import com.saehyun.a09_android.viewModelFactory.PostGetViewModelFactory
+import com.saehyun.a09_android.viewModelFactory.PostOtherViewModelFactory
 
 class PostActivity : AppCompatActivity() {
 
@@ -19,13 +27,22 @@ class PostActivity : AppCompatActivity() {
 
     private lateinit var postGetViewModelFactory: PostGetViewModelFactory
 
+    private lateinit var postOtherViewModel: PostOtherViewModel
+
+    private lateinit var postOtherViewModelFactory: PostOtherViewModelFactory
+
+    private var productList = arrayListOf<PostOtherResponse>()
+
+    private val TAG = "PostActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityPostBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val repository = Repository()
+        // PostGet
+        var repository = Repository()
 
         postGetViewModelFactory = PostGetViewModelFactory(repository)
         postGetViewModel = ViewModelProvider(this, postGetViewModelFactory).get(PostGetViewModel::class.java)
@@ -53,5 +70,33 @@ class PostActivity : AppCompatActivity() {
 
         val postId = intent.getStringExtra("postId").toString().toInt()
         postGetViewModel.authGetPost(postId)
+
+        // Recyclerview set
+        binding.rvPostOtherProduct.layoutManager = GridLayoutManager(this, 3)
+        binding.rvPostOtherProduct.setHasFixedSize(true)
+        binding.rvPostOtherProduct.adapter = RcOtherRvAdapter(applicationContext, productList)
+
+        // 다른 상품
+        postOtherViewModelFactory = PostOtherViewModelFactory(repository)
+        postOtherViewModel = ViewModelProvider(this, postOtherViewModelFactory).get(PostOtherViewModel::class.java)
+
+        postOtherViewModel.authPostOtherResponse.observe(this, Observer {
+            if (it.isSuccessful) {
+                productList.clear()
+                for (i: Int in 0..5) {
+                    productList.add(it.body()!!.get(i))
+                    binding.rvPostOtherProduct.adapter?.notifyDataSetChanged()
+                }
+            } else {
+                when (it.code()) {
+                    401 -> ToastUtil.print(applicationContext, "비밀번호가 일치하지 않습니다.")
+                    404 -> ToastUtil.print(applicationContext, "회원이 존재하지 않습니다.")
+                }
+            }
+        })
+
+        postOtherViewModel.authPostOther()
+
+
     }
 }
