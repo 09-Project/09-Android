fecd101b8803866b94c8d78637eafb01b703a38f
limsaehyun <sh00710000@gmail.com>
2021-11-03 22:21:15 +0900

    Fix :: type

--- 09-Android/app/src/main/java/com/saehyun/a09_android/model/request/PostPostRequest.kt
+++ 09-Android/app/src/main/java/com/saehyun/a09_android/model/request/PostPostRequest.kt
@@ -1,10 +1,13 @@
 package com.saehyun.a09_android.model.request
 
+import okhttp3.MultipartBody
+import java.io.File
+
 data class PostPostRequest(
     val title: String,
     val content: String,
     val price: Int,
     val transactionRegion: String,
     val openChatLink: String,
-    val image: String
+    val image: MultipartBody.Part
 )
