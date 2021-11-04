4bfc2d501e92a0f6644ec94002120032c2108453
limsaehyun <sh00710000@gmail.com>
2021-11-04 19:38:11 +0900

    Feat :: MyPageResponse

---
+++ 09-Android/app/src/main/java/com/saehyun/a09_android/model/response/MyPageResponse.kt
@@ -0,0 +1,12 @@
+package com.saehyun.a09_android.model.response
+
+data class MyPageResponse(
+    val name: String,
+    val profile_url: String,
+    val introduction: String,
+    val all_posts_count: String,
+    val get_likes_count: String,
+    val in_progress_posts_count: String,
+    val completed_posts_count: String,
+    val like_posts_count: String
+)
