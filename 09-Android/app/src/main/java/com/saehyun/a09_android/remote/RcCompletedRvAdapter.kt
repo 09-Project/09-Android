535d47bfcf24102e321e0fe2e6807e555d0bbf13
saehyun <sh00710000@gmail.com>
2021-11-09 10:57:18 +0900

    Feat :: 완료된 게시물 RvAdapter

---
+++ 09-Android/app/src/main/java/com/saehyun/a09_android/remote/RcCompletedRvAdapter.kt
@@ -0,0 +1,76 @@
+package com.saehyun.a09_android.remote
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.saehyun.a09_android.R
+import com.saehyun.a09_android.model.data.PostValue
+import com.saehyun.a09_android.ui.activity.PostActivity
+import com.saehyun.a09_android.viewModel.PostLikeViewModel
+
+class RcCompletedRvAdapter(val context: Context, val productData: List<PostValue>, val postLikeViewModel: PostLikeViewModel):
+    RecyclerView.Adapter<RcCompletedRvAdapter.Holder>() {
+
+
+    private val TAG = "RcProductRvAdapter"
+
+    inner class Holder(itemView: View?) : RecyclerView.ViewHolder(itemView!!) {
+        val clRecommendedProduct =
+            itemView?.findViewById<ConstraintLayout>(R.id.clRecommendedProduct)
+        val ivImage = itemView?.findViewById<ImageView>(R.id.ivImage)
+        val tvTitle = itemView?.findViewById<TextView>(R.id.tvTitle)
+        val tvTransactionRegion = itemView?.findViewById<TextView>(R.id.tvTransactionRegion)
+        val tvPrice = itemView?.findViewById<TextView>(R.id.tvPrice)
+        val ivHeart = itemView?.findViewById<ImageView>(R.id.ivHeart)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view =
+            LayoutInflater.from(context).inflate(R.layout.product_completed_view, parent, false)
+        return Holder(view)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+
+        var data: PostValue = productData.get(position)
+
+        Glide.with(holder.itemView.context)
+                .load(data.image)
+                .into(holder.ivImage)
+
+        holder.tvTitle?.text = data.title
+        holder.tvTransactionRegion?.text = data.transaction_region
+        holder.tvPrice?.text = data.price
+
+        holder.ivHeart?.setOnClickListener {
+            holder.ivHeart.setImageResource(R.drawable.ic_heart_on)
+            postLikeViewModel.authPostLikeSearch(data.id.toInt())
+        }
+
+        if(data.liked) {
+            Glide.with(holder.itemView.context)
+                .load(R.drawable.ic_heart_on)
+                .into(holder.ivHeart)
+        }
+
+        holder.clRecommendedProduct?.setOnClickListener {
+            var intent = Intent(holder.itemView?.context, PostActivity::class.java)
+            intent.putExtra("postId", data.id)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            ContextCompat.startActivity(holder.itemView.context, intent, null)
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return productData.size
+    }
+}
