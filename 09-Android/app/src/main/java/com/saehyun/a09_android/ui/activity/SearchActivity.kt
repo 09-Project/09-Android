0628eee5939d4ec5e646db70ada70bcce2277984
limsaehyun <sh00710000@gmail.com>
2021-11-15 09:58:08 +0900

    Feat :: Search DrawerNavigation

--- 09-Android/app/src/main/java/com/saehyun/a09_android/ui/activity/SearchActivity.kt
+++ 09-Android/app/src/main/java/com/saehyun/a09_android/ui/activity/SearchActivity.kt
@@ -1,10 +1,10 @@
 package com.saehyun.a09_android.ui.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
 import android.view.View
-import androidx.core.view.GravityCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
@@ -13,14 +13,17 @@ import com.saehyun.a09_android.databinding.ActivitySearchBinding
 import com.saehyun.a09_android.model.data.PostValue
 import com.saehyun.a09_android.remote.RcProductRvAdapter
 import com.saehyun.a09_android.repository.Repository
+import com.saehyun.a09_android.util.REFRESH_TOKEN
 import com.saehyun.a09_android.util.ToastUtil
 import com.saehyun.a09_android.util.VIEW_SIZE
 import com.saehyun.a09_android.viewModel.PostDeleteLikeViewModel
 import com.saehyun.a09_android.viewModel.PostLikeViewModel
 import com.saehyun.a09_android.viewModel.PostSearchViewModel
+import com.saehyun.a09_android.viewModel.ReissueViewModel
 import com.saehyun.a09_android.viewModelFactory.PostDeleteLikeViewModelFactory
 import com.saehyun.a09_android.viewModelFactory.PostLikeViewModelFactory
 import com.saehyun.a09_android.viewModelFactory.PostSearchViewModelFactory
+import com.saehyun.a09_android.viewModelFactory.ReissueViewModelFactory
 
 class SearchActivity : AppCompatActivity() {
 
@@ -41,6 +44,9 @@ class SearchActivity : AppCompatActivity() {
     private lateinit var postDeleteLikeViewModel: PostDeleteLikeViewModel
     private lateinit var postDeleteLikeViewModelFactory: PostDeleteLikeViewModelFactory
 
+    private lateinit var reissueViewModelFactory: ReissueViewModelFactory
+    private lateinit var reissueViewModel: ReissueViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -48,6 +54,9 @@ class SearchActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val repository = Repository()
+
+        val reissueViewModelFactory = ReissueViewModelFactory(repository,applicationContext)
+        val reissueViewModel = ViewModelProvider(this,reissueViewModelFactory).get(ReissueViewModel::class.java)
 
         keyword = intent.getStringExtra("keyword").toString()
 
@@ -63,8 +72,7 @@ class SearchActivity : AppCompatActivity() {
                 ToastUtil.print(applicationContext, "찜하기 성공!")
             } else {
                 when(it.code()) {
-                    400 -> ToastUtil.print(applicationContext, "Access 토큰의 형태가 잘못되었습니다.")
-                    401 -> ToastUtil.print(applicationContext, "Access 토큰이 유효하지 않습니다.")
+                    401 -> reissueViewModel.authReissue(REFRESH_TOKEN)
                     404 -> ToastUtil.print(applicationContext, "상품이나 회원이 존재하지 않습니다.")
                     409 -> ToastUtil.print(applicationContext, "찜이 이미 존재합니다.")
                 }
@@ -82,7 +90,7 @@ class SearchActivity : AppCompatActivity() {
             } else {
                 when(it.code()) {
                     401 -> {
-                        // 토큰 만료
+                        reissueViewModel.authReissue(REFRESH_TOKEN)
                     }
                     404 -> {
                         ToastUtil.print(applicationContext, "상품 또는 회원이 존재하지 않습니다.")
@@ -124,6 +132,8 @@ class SearchActivity : AppCompatActivity() {
                 val count = it.body()!!.count
                 maxPage = count / 16
                 if(count % 16 != 0) maxPage++
+                if(maxPage == 0) maxPage++
+
                 binding.tvSearchCurrentPage.text = "${currentPage+1}"
                 binding.tvSearchMaxPage.text = " / ${maxPage}"
 
@@ -143,11 +153,55 @@ class SearchActivity : AppCompatActivity() {
                 }
             } else {
                 when(it.code()) {
+                    401 -> reissueViewModel.authReissue(REFRESH_TOKEN)
                     404 -> ToastUtil.print(applicationContext,"이미지가 존재하지 않습니다")
                 }
             }
         })
 
         searchViewModel.authPostSearch(keyword, currentPage, VIEW_SIZE)
+
+        // Search
+        binding.seditText.setOnClickListener {
+            val keyword = binding.seditText.text.toString()
+
+            if(keyword.isEmpty()) {
+                ToastUtil.print(applicationContext, "검색어를 입력해주세요")
+                return@setOnClickListener
+            }
+
+            val intent: Intent = Intent(this, SearchActivity::class.java)
+            intent.putExtra("keyword", keyword)
+            startActivity(intent)
+        }
+
+        // Drawer Menu
+        binding.ibSearchMenu.setOnClickListener {
+            if(binding.searchDrawer.isDrawerOpen(Gravity.RIGHT)) {
+                binding.searchDrawer.closeDrawer(Gravity.RIGHT)
+            } else {
+                binding.searchDrawer.openDrawer(Gravity.RIGHT)
+            }
+        }
+
+        binding.searchNavi.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.menuHome -> {
+                    val intent = Intent(applicationContext, MainActivity::class.java)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                    startActivity(intent)
+                    return@setNavigationItemSelectedListener true
+                }
+                R.id.menuPost -> {
+                    startActivity(Intent(applicationContext, CreatePostActivity::class.java))
+                    return@setNavigationItemSelectedListener true
+                }
+                R.id.menuMyPage -> {
+                    startActivity(Intent(applicationContext, MemberShowActivity::class.java))
+                    return@setNavigationItemSelectedListener true
+                }
+                else -> return@setNavigationItemSelectedListener false
+            }
+        }
     }
 }
