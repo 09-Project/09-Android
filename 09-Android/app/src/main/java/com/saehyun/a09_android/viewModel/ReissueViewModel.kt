511265f49648390d3555ad2bd1dc0618aac92c79
limsaehyun <sh00710000@gmail.com>
2021-11-15 09:59:03 +0900

    Feat :: 토큰 만료 시 로그아웃

--- 09-Android/app/src/main/java/com/saehyun/a09_android/viewModel/ReissueViewModel.kt
+++ 09-Android/app/src/main/java/com/saehyun/a09_android/viewModel/ReissueViewModel.kt
@@ -1,33 +1,46 @@
 package com.saehyun.a09_android.viewModel
 
+import android.content.Context
+import android.content.Intent
+import android.os.Handler
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.saehyun.a09_android.model.request.AuthLoginRequest
-import com.saehyun.a09_android.model.response.AuthLoginResponse
 import com.saehyun.a09_android.model.response.AuthReissueResponse
 import com.saehyun.a09_android.repository.Repository
+import com.saehyun.a09_android.ui.activity.LoginActivity
+import com.saehyun.a09_android.util.ACCESS_TOKEN
 import com.saehyun.a09_android.util.REFRESH_TOKEN
 import com.saehyun.a09_android.util.ToastUtil
 import kotlinx.coroutines.launch
 import retrofit2.Response
 
-class ReissueViewModel(private val repository: Repository) : ViewModel() {
+class ReissueViewModel(private val repository: Repository, private val context: Context) : ViewModel() {
     val authReissueResponse : MutableLiveData<Response<AuthReissueResponse>> = MutableLiveData()
-    val toastMessage = MutableLiveData<String>()
-    val refreshTokenExpiration = MutableLiveData<Boolean>()
 
-    fun authReissue(refreshToken : String) {
+    fun authReissue(refreshToken: String) {
         viewModelScope.launch {
             val response = repository.authReissue(refreshToken)
             authReissueResponse.value = response
 
             if (response.isSuccessful) {
-                toastMessage.value = "토큰 재발급에 성공하셨습니다."
+                REFRESH_TOKEN = response.body()?.refreshToken.toString()
+                ACCESS_TOKEN = response.body()?.accessToken.toString()
             } else {
                 when (response.code()) {
-                    401 -> refreshTokenExpiration.value = true
-                    404 -> toastMessage.value = "예기치 못한 오류가 발생했습니다.\n고객센터에 문의해주세요."
+                    401 -> {
+                        ToastUtil.print(context, "토큰이 만료되어 로그아웃 되었습니다.")
+
+                        Handler().postDelayed({
+                            val intent = Intent(context, LoginActivity::class.java)
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                            ACCESS_TOKEN = "default"
+                            REFRESH_TOKEN = "default"
+                            context.startActivity(intent)
+                        }, 2000L)
+
+                    }
+                    404 -> ToastUtil.print(context, "예기치 못한 오류가 발생했습니다.\n고객센터에 문의해주세요.")
                 }
             }
         }
