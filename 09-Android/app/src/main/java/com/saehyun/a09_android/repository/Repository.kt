011b24e692eb0f075c577d259a365dda29b56bed
limsaehyun <sh00710000@gmail.com>
2021-11-15 20:17:13 +0900

    Feat :: PostComplete repository

--- 09-Android/app/src/main/java/com/saehyun/a09_android/repository/Repository.kt
+++ 09-Android/app/src/main/java/com/saehyun/a09_android/repository/Repository.kt
@@ -11,90 +11,88 @@ import okhttp3.RequestBody
 import retrofit2.Response
 
 class Repository {
-    private val TAG = "Repository"
-
-    suspend fun authSignUP(name: String, username: String, password: String) : Response<Void> {
+    suspend fun authSignUP(name: String, username: String, password: String): Response<Void> {
         val authSignUpRequest = AuthSignUpRequest(name, username, password)
         return ApiProvider.api.authSignUp(authSignUpRequest)
     }
 
-    suspend fun authLogin(username : String, password : String) : Response<AuthLoginResponse> {
+    suspend fun authLogin(username: String, password: String): Response<AuthLoginResponse> {
         val authLoginRequest = AuthLoginRequest(username, password)
         return ApiProvider.api.authLogin(authLoginRequest)
     }
 
-    suspend fun authReissue(refreshToken: String) : Response<AuthReissueResponse> {
+    suspend fun authReissue(refreshToken: String): Response<AuthReissueResponse> {
         return ApiProvider.api.authReissue(refreshToken)
     }
 
-    suspend fun post(page: Int, size: Int) : Response<PostResponse> {
+    suspend fun post(page: Int, size: Int): Response<PostResponse> {
         val tempToken: String = "Bearer " + ACCESS_TOKEN
         return ApiProvider.api.post(tempToken, page, size)
     }
 
-    suspend fun searchPost(keyword: String, page: Int, size: Int) : Response<PostResponse> {
+    suspend fun searchPost(keyword: String, page: Int, size: Int): Response<PostResponse> {
         return ApiProvider.api.searchPost(keyword, page, size)
     }
 
-    suspend fun postOther() : Response<List<PostOtherResponse>> {
+    suspend fun postOther(): Response<List<PostOtherResponse>> {
         val tempToken: String = "Bearer " + ACCESS_TOKEN
         return ApiProvider.api.otherPost(tempToken)
     }
 
-    suspend fun postGet(pageId: Int) : Response<PostGetResponse> {
+    suspend fun postGet(pageId: Int): Response<PostGetResponse> {
         val tempToken: String = "Bearer " + ACCESS_TOKEN
         return ApiProvider.api.getPost(tempToken, pageId)
     }
 
     suspend fun postGroupBuy(title: RequestBody, content: RequestBody, price: RequestBody, transactionRegion: RequestBody,
-                              openChatLink: RequestBody, image: MultipartBody.Part) : Response<Void> {
+                             openChatLink: RequestBody, image: MultipartBody.Part): Response<Void> {
         val tempToken: String = "Bearer " + ACCESS_TOKEN
         return ApiProvider.api.postGroupBuy(tempToken, title, content, price, transactionRegion, openChatLink, image)
     }
 
     suspend fun postSharing(title: RequestBody, content: RequestBody, transactionRegion: RequestBody,
-                             openChatLink: RequestBody, image: MultipartBody.Part) : Response<Void> {
+                            openChatLink: RequestBody, image: MultipartBody.Part): Response<Void> {
         val tempToken: String = "Bearer " + ACCESS_TOKEN
         return ApiProvider.api.postSharing(tempToken, title, content, transactionRegion, openChatLink, image)
     }
 
-    suspend fun postLike(postId: Int) : Response<Void> {
+    suspend fun postLike(postId: Int): Response<Void> {
         val tempToken: String = "Bearer " + ACCESS_TOKEN
         return ApiProvider.api.likePost(tempToken, postId)
     }
 
-    suspend fun deleteLikePost(postId: Int) : Response<Void> {
+    suspend fun deleteLikePost(postId: Int): Response<Void> {
         val tempToken: String = "Bearer " + ACCESS_TOKEN
         return ApiProvider.api.deleteLikePost(tempToken, postId)
     }
 
-    suspend fun membertLike() : Response<List<PostValue>> {
+    suspend fun membertLike(): Response<List<PostValue>> {
         val tempToken: String = "Bearer " + ACCESS_TOKEN
         return ApiProvider.api.membertLike(tempToken)
     }
 
-    suspend fun myPage() : Response<MemberShowResponse> {
-        val tempToken: String = "Bearer " + ACCESS_TOKEN
-        return ApiProvider.api.myPage(tempToken)
-    }
-
-    suspend fun memberInProgress(memberId: String) : Response<List<PostValue>> {
+    suspend fun memberInProgress(memberId: String): Response<List<PostValue>> {
         val tempToken: String = "Bearer " + ACCESS_TOKEN
         return ApiProvider.api.memberInProgress(tempToken, memberId)
     }
 
-    suspend fun memberCompleted(memberId: String) : Response<List<PostValue>> {
+    suspend fun memberCompleted(memberId: String): Response<List<PostValue>> {
         val tempToken: String = "Bearer " + ACCESS_TOKEN
         return ApiProvider.api.memberCompleted(tempToken, memberId)
     }
 
-    suspend fun postDelete(postId: String) : Response<Void> {
+    suspend fun postDelete(postId: String): Response<Void> {
         val tempToken: String = "Bearer " + ACCESS_TOKEN
         return ApiProvider.api.postDelete(tempToken, postId)
     }
 
-    suspend fun memberShow(memberId: String) : Response<MemberShowResponse> {
+    suspend fun memberShow(memberId: String): Response<MemberShowResponse> {
         val tempToken: String = "Bearer " + ACCESS_TOKEN
         return ApiProvider.api.memberShow(tempToken, memberId)
+    }
+
+    suspend fun postComplete(postId: String): Response<Void> {
+        val tempToken: String = "Bearer " + ACCESS_TOKEN
+        return ApiProvider.api.postComplete(tempToken, postId)
     }
 }
